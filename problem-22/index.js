9156462911fff402ba0992a64e9bc521d2bac6a8
OsaidF <osaidfarooqui@outlook.com>
2024-03-01 19:34:53 +0500

    forgot to add code for problem 22

--- problem-22/index.js
+++ problem-22/index.js
@@ -1,12 +1,8 @@
-/* Intentional Error: If you haven’t received an array index error in one of your programs yet, 
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet,
 try to make one happen. Change an index in one of your programs to produce an index error.
  Make sure you correct the error before closing the program. */
-
-let x;
+var x;
 console.log(x);
-
 // calling a variable before assigning a value to it gives an index error?
-
-x = "new"
-
+x = "new";
 console.log(x);
